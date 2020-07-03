20fa8f8c78b9cdb1a3542e0e8a3de3dbb7aa61e9
chris_MSI1 <chris.h.felt@gmail.com>
2020-07-03 12:44:38 -0600

    adding list model

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class List(models.Model):
+    username = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_lenth=50)
+    color = models.CharField(max_length=10)
+    
